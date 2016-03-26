86857c90ed047cac05fa540a8c8118e10cdee631
aa6my <aa6my@qq.com>
2016-03-27 04:36:27 +0800

    example and tweaks

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,6 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// use \Illuminate\Database\Eloquent\Model as Eloquent;
+// use \Illuminate\Database\Capsule\Manager as Capsule;
+
+// class tablename(s) extends Illuminate\Database\Eloquent\Model {}
+
 class Welcome extends MY_Controller {
 
 	/**
@@ -18,7 +23,6 @@ class Welcome extends MY_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	
 	public function index()
 	{
 	    $this->twig->render('welcome_message', array(
@@ -27,6 +31,22 @@ class Welcome extends MY_Controller {
 	        'elapsed_time' => $this->benchmark->elapsed_time(),
 	        'version' => (ENVIRONMENT === 'development') ? CI_VERSION : '',
 	    ));
+
+	    // https://laravel.com/docs/5.2/queries or https://laravel.com/docs/4.2/eloquent
+
+	    // eloquent ORM
+	    //$ss = Attendance::select('nama', 'email')->where('att_d1', '=', 1)->get();
+
+	    // suggest user capsule
+	    //$users = Capsule::table('attendance')->where('att_d1', '=', 1)->get();
+
+	    //echo $att->nama;
+	    //Console::log($att);
+
+	    //foreach ($users as $user)
+		//{
+		//    echo $user->email;
+		//}
 
 	    // Redis Cache Driver If Enabled
 	    // if ($this->cache->redis->is_supported()) {
